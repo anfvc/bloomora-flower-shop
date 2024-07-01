c2395b02d396bc9022aac7091cdfa1a3c4bb3233
ilkckn <musa.cekcen@dci-student.org>
2024-07-01 16:11:00 +0200

    part of the landing page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Home from "./views/home/Home.jsx";
+import Shop from "./views/shop/Shop.jsx"
+import About from "./views/about/About.jsx"
+import Blog from "./views/blog/Blog.jsx"
+import Contact from "./views/contact/Contact.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/data/landingPageSlider.js
@@ -0,0 +1,21 @@
+import image1 from "../images/home-images/image1.jpg";
+import image2 from "../images/home-images/image2.jpg";
+import image3 from "../images/home-images/image3.jpg";
+
+export const landingSlider = [
+  {
+    image: image1,
+    title1: "shop online or visit our store",
+    title2: "10 station st wentworth falls"
+  },
+  {
+    image: image2,
+    title1: "Bloomora is there to support you in every special moment.",
+    title2: "beautiful flowers for every occasion"
+  },
+  {
+    image: image3,
+    title1: "Bloomora is there to support you in every special moment.",
+    title2: "beautiful flowers for every occasion"
+  },
+]
