bddae458c5db2b26195fa36a999b90e32652430f
andresvillay <andres.villay@dci-student.org>
2024-07-01 15:04:29 +0200

    added data.js in client side

---
+++ client/src/data.js
@@ -0,0 +1,255 @@
+//Decor - Artificial
+import apple from "./images/decor/artificial/apple_bouq.jpg";
+import fl_b from "./images/decor/artificial/fl_b.jpg";
+import fl_b1 from "./images/decor/artificial/fl_b1.jpg";
+import fl_b2 from "./images/decor/artificial/fl_b2.jpg";
+import fl_b3 from "./images/decor/artificial/fl_b3.jpg";
+import magnolia_p from "./images/decor/artificial/magnolia_p.jpg";
+import magnolia_sp from "./images/decor/artificial/magnolia_sp.jpg";
+import magnolia from "./images/decor/artificial/magnolia.jpg";
+import peonies_b from "./images/decor/artificial/peonies_b.jpg";
+import peonies from "./images/decor/artificial/peonies.jpg";
+import roses from "./images/decor/artificial/roses.jpg";
+//Decor - Christmas
+import children from "./images/decor/christmas/children.jpg";
+import chr_comp from "./images/decor/christmas/chr_comp.jpg";
+import chr_tree from "./images/decor/christmas/chr_tree.jpg";
+import chr_tree2 from "./images/decor/christmas/chr_tree.jpg";
+import cup from "./images/decor/christmas/cup.jpg";
+import deer from "./images/decor/christmas/deer.jpg";
+import garl_tree from "./images/decor/christmas/garl_tree.jpg";
+import gnome from "./images/decor/christmas/gnome.jpg";
+import golden_deer from "./images/decor/christmas/golden_deer.jpg";
+import mug from "./images/decor/christmas/mug.jpg";
+import ny_comp from "./images/decor/christmas/ny_comp.jpg";
+import ny_wteath from "./images/decor/christmas/ny_wteath.jpg";
+import ny_wteath2 from "./images/decor/christmas/ny_wteath2.jpg";
+//Decor - For Home
+
+
+export const products = [
+  {
+    artificial: [
+      {
+        name: "Apple Bouquet",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "50",
+        type: "Artificial",
+        category: "Decor",
+        image: apple,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Fl_b",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "70",
+        type: "Artificial",
+        category: "Decor",
+        image: fl_b,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Fl_b1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "60",
+        type: "Artificial",
+        category: "Decor",
+        image: fl_b1,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Fl_b2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        type: "Artificial",
+        price: "60",
+        category: "Decor",
+        image: fl_b2,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Fl_b3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        type: "Artificial",
+        price: "50",
+        category: "Decor",
+        image: fl_b3,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Magnolia P",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "70",
+        category: "Decor",
+        image: magnolia_p,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Magnolia SP",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        type: "Artificial",
+        price: "60",
+        category: "Decor",
+        image: magnolia_sp,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Magnolia",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        type: "Artificial",
+        price: "50",
+        category: "Decor",
+        image: magnolia,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Peonies B",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        type: "Artificial",
+        price: "50",
+        category: "Decor",
+        image: peonies_b,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Peonies",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        type: "Artificial",
+        price: "50",
+        category: "Decor",
+        image: peonies,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Roses",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        type: "Artificial",
+        price: "50",
+        category: "Decor",
+        image: roses,
+        size: ["Small", "Medium", "Large"],
+      },
+    ],
+  },
+  {
+    christmas: [
+      {
+        name: "Children Figure",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "50",
+        type: "Christmas",
+        category: "Decor",
+        image: children,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Christmas Composition",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "80",
+        type: "Christmas",
+        category: "Decor",
+        image: chr_comp,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Christmas Tree",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "50",
+        type: "Christmas",
+        category: "Decor",
+        image: chr_tree,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Snowy Christmas Tree",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "60",
+        type: "Christmas",
+        category: "Decor",
+        image: chr_tree2,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Christmas Cup with Lid",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "70",
+        type: "Christmas",
+        category: "Decor",
+        image: cup,
+        size: ["Small", "Medium", "Large"],
+        color: ["Dark Blue", "Pink", "Red"],
+      },
+      {
+        name: "Christmas Deer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "30",
+        type: "Christmas",
+        category: "Decor",
+        image: deer,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Garland Tree",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "30",
+        type: "Christmas",
+        category: "Decor",
+        image: garl_tree,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Gnome",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "45",
+        type: "Christmas",
+        category: "Decor",
+        image: gnome,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Golden Deer Figure",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "60",
+        type: "Christmas",
+        category: "Decor",
+        image: golden_deer,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Christmas Mug",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "35",
+        type: "Christmas",
+        category: "Decor",
+        image: mug,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "New Year's Centerpiece",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "55",
+        type: "Christmas",
+        category: "Decor",
+        image: ny_comp,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "New Year's Wreath",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "65",
+        type: "Christmas",
+        category: "Decor",
+        image: ny_wteath,
+        size: ["Small", "Medium", "Large"],
+      },
+      {
+        name: "Wreath with New Year's Toys and Pine Cones",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis interdum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce facilisis lorem dui, eu volutpat ex faucibus vel",
+        price: "65",
+        type: "Christmas",
+        category: "Decor",
+        image: ny_wteath2,
+        size: ["Small", "Medium", "Large"],
+      },
+    ],
+  },
+];
