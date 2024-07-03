3c49c0893505c357f5a73e35fb48ff9b861a32d1
Zhanna <zhanna.chalohlu@dci-student.org>
2024-07-03 10:48:03 +0200

    3c

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import 'express-async-errors'
-import mongoose from 'mongoose'
 import authRouter from './routes/authRouter.js'
 import cookieParser from 'cookie-parser'
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js'
 
-
 const app = express()
 
+await connection();
+
+app.use(cors());
 app.use(express.json())
 app.use(cookieParser())
 
@@ -18,14 +19,13 @@ app.use('*', (req, res)=>{
 })
 
 app.use(errorHandlerMiddleware)
-const port = process.env.PORT || 5100
+const port = process.env.PORT || 5100;
 
-try {
-    await mongoose.connect(process.env.MONGO_URL);
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}...`);
-    })
-} catch (error) {
-    console.log(error);
-    process.exit(1)
-}
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+
