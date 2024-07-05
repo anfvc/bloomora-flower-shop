e597af3c335ec55880df3f79d5f2ab6d5b0fd77e
ilkckn <musa.cekcen@dci-student.org>
2024-07-05 11:19:00 +0200

    added effect to the liked button and so on

--- client/src/views/shop/Shop.jsx
+++ client/src/views/shop/Shop.jsx
@@ -1,11 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./Shop.css";
 import { UserContext } from "../../context/userContext.jsx";
 import SortFilter from "../../components/sort-filter/SortFilter.jsx";
 import { CiHeart } from "react-icons/ci";
+import { IoMdHeart } from "react-icons/io";
 
 function Shop() {
   const { sortedProducts } = useContext(UserContext);
+  const [likedItems, setLikedItems] = useState(
+    new Array(sortedProducts.length).fill(false)
+  );
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+
+  function handleLike(index) {
+    const newLikedItems = [...likedItems];
+    newLikedItems[index] = !newLikedItems[index];
+    setLikedItems(newLikedItems);
+  }
+
+  function handleMouseEnter(index) {
+    setHoveredIndex(index);
+  }
+
+  function handleMouseLeave() {
+    setHoveredIndex(-1);
+  }
 
   return (
     <div className="shopContainer">
@@ -17,8 +36,19 @@ function Shop() {
             <div className="imageBox">
               <img src={item.image} alt={item.name} />
               <button className="addToCart">add to cart</button>
-              <div className="likeButton">
-                <CiHeart />
+              <div
+                className="likeButton"
+                onClick={() => handleLike(index)}
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}
+              >
+                {likedItems[index] ? (
+                  <IoMdHeart style={{ color: "darkred" }} />
+                ) : hoveredIndex === index ? (
+                  <IoMdHeart style={{ color: "white" }} />
+                ) : (
+                  <CiHeart style={{ color: "white" }} />
+                )}
               </div>
             </div>
             <div className="info">
